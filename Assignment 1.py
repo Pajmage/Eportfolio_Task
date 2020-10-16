fc91490b4fc69435f3c0e7cdec133c9abbf611b2
Pajmage <50663754+Pajmage@users.noreply.github.com>
2020-10-16 16:01:47 +0100

    Update Assignment 1.py

--- Assignment 1.py
+++ Assignment 1.py
@@ -1,139 +1,46 @@
 '''
-EDUCATION OOP
-
-- SCHOOL DETAILS tick
-- CHILD DETAILS
-- GRADES
-
+MSc Computer Systems and Management
+CSTM65 Software Engineering Principles
+Author: Paul Jones
+Date: 10/10/2020
 '''
 
-class School():
-    def __init__(self, schoolName, location):
-        self.schoolName = schoolName
-        self.location = location
-        print('(Initialized School Class: {})'.format(self.schoolName))
-        print('(Initialized School Class: {})'.format(self.location))
 
-    def getSchoolName(self):
-        return self.schoolName
+class River(): #Defines a class in python, in this case the class is called 'River'
 
-    def getSchoolLocation(self):
-        return self.location
+    Canoeing = "Yes" # this is a class vriable.  Every instance of the River class will have this attribute
 
+    def __init__(self, RiverName, Location, Fishing): # This initialises the class to have initial values of RiverName and Location
+        self.RiverName = RiverName #sets the attributes for each instance of the class
+        self.Location = Location #sets the attributes for each instance of the class
+        self.Fishing = Fishing #sets the attributes for each instance of the class
+
+    def FishingIssue(self):
+        if self.Fishing == True:
+            return f"{self.RiverName} has fishing so be wary of fishermen"
+        else:
+            return f"{self.RiverName} has no fishing so have fun!"
     
     def __str__(self):
-        return f"School Name: {self.schoolName}. School Location: {self.location}"
-    
-    def __repr__(self):
-        return f"{self.schoolName!r}, {self.location!r}"
-  
-print(repr(School))
-
-
-    #Edu = School("Royal Grammar School",
-               #"Newcastle Upon Tyne- Jesmond")
-
-    #print(Edu)
-    #print(repr(School))
-
-
-    #2nd class about the child attending the School
-class Child(School):
-    def __init__(self, name, age, yeargroup, schoolName, location):
-        self.name = name
-        self.age = age
-        self.yeargroup = yeargroup
-        super().__init__(schoolName, location)
-
-        #super().__init__(schoolName, location)
-        print('(Initialized Child Class: {})'.format(self.name))
-        print('(Initialized Child Class: {})'.format(self.age))
-        print('(Initialized Child Class: {})'.format(self.yeargroup))
-        print()
-        print(self.name + "'s item's '")
-        print()
-        
-
-        #School.__init__(self, schoolName, location)
-
-    # Access from inherited class where the child goes to School
-      #  super().__init__(schoolName, location)
-    
-    #Access composition class
-      #self.obj_overview = Overview(attendance, grade)
-    
-    def getChildName(self):
-        return self.name
-
-    def getChildAge(self):
-        return self.age
-
-    def getChildYearGroup(self):
-        return self.yeargroup
- #this string is displaying the information
-    #def __str__(self):
-      #return f"{self.name} is {self.age} and is currently is yearx  {self.yeargroup} and the school name is {self.location } {super().__str__()}"
-
-#Kieran = Child("Kieran", 12, 12, "ROYAL GRAMMAR SCHOOL", "Newcastle Upon Tyne")
-#print(Kieran)
-
- # currently this does nothing, how can i get this into the program?
-class Overview(Child, School):
-    def __init__(self, attendance, grade, name, age, yeargroup, schoolName, location):
-        self.attendance = attendance
-        self.grade = grade
-        super().__init__(name, age, yeargroup, schoolName, location)
-        #print('(Initialized Overview Class: {})'.format(self.name))
-
-    def getAttendance(self):
-        return self.attendance
-
-    def getGrade(self):
-        return self.grade
-    
-    # inherited
-    # __Str__ informal representation
-    def __str__(self):
-        return f"{self.name}s attendance has been oustanding, acheiving an overall attendance of: {self.attendance}. He is currently Aged: {self.age} and is in YearGroup:  {self.yeargroup} and his overall grade for the year is: {self.grade} - he attends the school in {self.location} and the name of the school is {self.schoolName}"
+        return f"River Name: {self.RiverName}. Location: {self.Location}"
 
     def __repr__(self):
-        return f"{self.__class__.__name__}{self.__dict__}"
-       
+        return "River()"
         
-        
-#{super().__str__()}"
-
-
-#prints a blank line
-print()
-# this one accepts the data to be passed 
-#person 1
-p1 = Overview(80, "A", "kieran", 8, 12, "Royal Grammar School", "Newcastle Upon Tyne")
-print(p1)
-#p1.schoolName = "ResourceWarning"
-print()
-p2 = Overview(20, "B", "Paj", 31, "University", "Sunderland University", "Sunderland")
-print(p2)
 print()
 
-#access repr string
-print()
-print("Testing the REPR")
-print()
-print(repr(p1))
-#print(p1.getSchoolName())
+class RiverDetails(River):
 
-#p1.name = "alan"
-#p1.age = "12"
-#p1.yeargroup = 8
-#p1.location = "nEWCASTLE"
-
-#print()
-#print(Overview.__bases__)
- #returns a list of all the members in the specified obkect
-#print(dir(p1))
+    def __init__(self, RiverLength, Grade):
+        self.RiverLength = RiverLength
+        self.Grade = Grade
+        super().__init__(RiverName, Location, Fishing)
 
 
-#person 2
-#p2 = Overview(10, 12, "james", 1, 5, "england", "stella")
-#print(Overview)
+    def Rating(self):
+        if self.Grade > 3:
+            return f"{self.RiverName} is rated Extreme"
+        elif self.Grade == 3:
+            return f"{self.RiverName} is rated HARD!"
+        elif self.Grade < 3:
+            return f"{self.RiverName} is rated Easy!"
